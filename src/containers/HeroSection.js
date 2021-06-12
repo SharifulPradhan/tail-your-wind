0930c7ecfb00849f5367beb37e8ae416da42778d
Shariful Pradhan Hridoy <sharifulpradhanhridoy@Gmail.com>
2021-06-12 22:51:53 +0600

    first commit

---
+++ src/containers/HeroSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+    <div style={{height:"450px"}} className='bg-gray-400 flex flex-col justify-center items-center container-sm'>
+      <div className="w-3/5">
+      <h1 className="text-5xl">The new standard in wealth management </h1>
+      <p className="font-mono mt-4">We deliver independent advice and thoughtfully designed wealth management solutions, free from the conflicts at many financial institutions. Our senior investment and planning professionals take the time to get to know you – to know your family, your business and your other interests – and to invest and plan for your future, on your terms</p>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
---
+++ src/containers/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export default function NavBar() {
+  return (
+    <>
+      <div className="bg-gray-200">
+        <nav className="bg-black text-center text-white" style={{height:'100px'}}>
+          <h1 className='text-6xl'>PROFI<span className="text-indigo-400">tex</span></h1>
+          <p className='font-mono'>investment Company</p>
+        </nav>
+      </div>
+    </>
+  );
+}

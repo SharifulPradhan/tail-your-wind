2c6fe73c7441e526e67b2331be5d9dcd21d5be45
Shariful Pradhan Hridoy <sharifulpradhanhridoy@Gmail.com>
2021-06-13 17:57:24 +0600

    some Bug fixed

--- src/containers/Aboutus.js
+++ src/containers/Aboutus.js
@@ -3,8 +3,8 @@ import aboutusImage from '../about-us.png';
 
 const Aboutus = () => {
   return (
-    <section className='bg-gray-100 flex flex-col justify-center items-center mt-9 p-5'>
-      <div className='w-3/5 flex justify-center items-center mobile:flex-col desktop:flex-row'>
+    <section className='bg-gray-100 flex flex-col justify-center items-center mt-9'>
+      <div className='w-3/5 flex justify-center items-center mobile:flex-col desktop:flex-row mt-5'>
         <div>
           <h1 className='desktop:text-5xl mobile:text-2xl'>About Tail your Wind</h1>
           <p className='text-lg mt-5'>We help organizations across the private, public, and social sectors create the Change that Matters most to them.</p>
--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = () => {
   return (
     <section className='flex justify-center bg-gray-500 p-5'>
-      <form class="desktop:w-2/5 mt-7 mb-7 mobile:w-full">
+      <form class="desktop:w-2/5 mobile:w-full">
         <div class="flex items-center border-b border-yellow-500 py-2">
           <input class="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none" type="email" placeholder="exampl@company.com" aria-label="Email" />
           <button class="flex-shrink-0 bg-green-500 hover:bg-yellow-400 border-yellow-400 hover:border-green-700 text-sm border-4 text-white hover:text-green-700 py-1 px-2 rounded" type="button">
